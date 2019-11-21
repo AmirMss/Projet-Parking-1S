68a6e0d7d0376463257314f9c1a5a9e1784c4935
Thomas Beguin <thom_78@hotmail.fr>
2019-11-21 23:55:00 +0100

    Oublie comm

--- src/main.c
+++ src/main.c
@@ -1,12 +1,10 @@
- #include "../inc/parking.h"
-
-
+#include "../inc/parking.h"
 
 static int	usage(char *name)
 {
 	printf("Usage : ./%s <map_file> <car_files ...>\nLes fichiers doivent être valide !\n", name);
 	return (0);
- }
+}
 
 int		main(int argc, char **argv)
 {
@@ -18,7 +16,8 @@ int		main(int argc, char **argv)
 		return (usage(argv[0]));
 	if ((map = prase_map(argv[1])) == NULL)
 		return (usage(arv[0]));
-	all_type = parse_car(argv);
+	
+	/*all_type = parse_car(argv);*/
 
 
 	return (0);
